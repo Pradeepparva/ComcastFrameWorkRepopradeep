8383c9cbd3765d8a457e61abf88f16f5b3df0f5f
Pradeepparva <pradeepparva@gmail.com>
2024-07-30 15:58:49 +0530

    Adding new classes

---
+++ seleniumMAVENandJENKINSpractice/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,17 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContact");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContact");
+	}
+
+}
---
+++ seleniumMAVENandJENKINSpractice/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,27 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		String URL=System.getProperty("url","http://localhost:8888");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username","admin");
+		String PASSWORD=System.getProperty("password","admin");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("execute createOrg");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrg");
+	}
+
+}
